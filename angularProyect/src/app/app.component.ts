ee5a2d380ab081e8ef3458b29954182536c76501
migcabvid <miguelcaballerovidal@gmail.com>
2025-01-15 15:38:04 +0100

    Creacion de la directiva ngStyle

--- angularProyect/src/app/app.component.ts
+++ angularProyect/src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angularProyect'; 
+  title = 'angularProyect';
 
-  a
+  mostrarMenu: boolean = false;
+  fMostrarMenu() {
+    this.mostrarMenu = !this.mostrarMenu;
+  }
+
+  colorEstado = {
+    "color": "red",
+    "background-color": "blue"
+  };
 }
