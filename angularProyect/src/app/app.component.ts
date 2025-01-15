1c7a8a131410ae72e6cd9d27e01ff25ef41aa488
Miguel Cantalejo <96992389+Migue025@users.noreply.github.com>
2025-01-15 17:00:42 +0100

    Creación y modificación de las directivas ngIf y ngStyle

--- angularProyect/src/app/app.component.ts
+++ angularProyect/src/app/app.component.ts
@@ -1,23 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'angularProyect';
 
-  mostrarMenu: boolean = false;
-  fMostrarMenu() {
-    this.mostrarMenu = !this.mostrarMenu;
-  }
+export class AppComponent {
+  mostrarTexto: boolean = true;
 
   colorEstado = {
     "color": "red",
-    "background-color": "blue"
+    "background-color": "blue",
+    "text-align": "center"
   };
 }
