e6ee7a21a1d7fc85152913bce7cb9215252f219b
Miguel Cantalejo <96992389+Migue025@users.noreply.github.com>
2025-01-31 19:06:33 +0100

    Formulario básico reactivo

--- angularProyect/src/app/app.component.ts
+++ angularProyect/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, HeaderComponent],
+  standalone: true, 
+  imports: [CommonModule, HeaderComponent, FooterComponent, FormularioComponent, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
