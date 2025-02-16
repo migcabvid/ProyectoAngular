021e5f4aa939376ee4baab2f6b35f49daf31c56c
migcabvid <miguelcaballerovidal@gmail.com>
2025-02-16 19:31:44 +0100

    intento de enrutamiento

--- angularProyect/src/app/app.routes.ts
+++ angularProyect/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HeaderComponent },
+  { path: 'formulario', component: FormularioComponent },
+];
