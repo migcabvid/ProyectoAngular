400e7325f5b3bc6ad26c19bab437cf72debdb2ef
Miguel Cantalejo <96992389+Migue025@users.noreply.github.com>
2025-02-17 11:46:26 +0100

    Routes + Quarkus Backend

--- angularProyect/src/app/app.routes.ts
+++ angularProyect/src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
+import { DirectivasComponent } from './directivas/directivas.component';
 import { FormularioComponent } from './formulario/formulario.component';
 
-export const routes: Routes = [
-  { path: '', component: HeaderComponent },
+
+export const appRoutes: Routes = [
+  // Carga DirectivasComponent al navegar a '/directivas'
+  { path: 'directivas', component: DirectivasComponent },
+  // Carga FormularioComponent al navegar a '/formulario'
   { path: 'formulario', component: FormularioComponent },
+  // Si la ruta está vacía, redirige a '/directivas'
+  { path: '', redirectTo: 'directivas', pathMatch: 'full' },
+  // Redirige cualquier ruta desconocida a '/directivas'
+  { path: '**', redirectTo: 'directivas' }
 ];
