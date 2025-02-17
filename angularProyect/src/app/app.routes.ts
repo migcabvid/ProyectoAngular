9461d1a6b8e241e212049361623833c7fb7a1746
migcabvid <miguelcaballerovidal@gmail.com>
2025-02-17 12:50:27 +0100

    creacion de controlador de acceso a datos

--- angularProyect/src/app/app.routes.ts
+++ angularProyect/src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { Routes } from '@angular/router';
 import { DirectivasComponent } from './directivas/directivas.component';
 import { FormularioComponent } from './formulario/formulario.component';
-
+import { DatosComponent } from './datos/datos.component';
 
 export const appRoutes: Routes = [
   // Carga DirectivasComponent al navegar a '/directivas'
   { path: 'directivas', component: DirectivasComponent },
   // Carga FormularioComponent al navegar a '/formulario'
   { path: 'formulario', component: FormularioComponent },
+  // Carga DatosComponent al navegar a '/datos'
+  { path: 'datos', component: DatosComponent },
   // Si la ruta está vacía, redirige a '/directivas'
   { path: '', redirectTo: 'directivas', pathMatch: 'full' },
   // Redirige cualquier ruta desconocida a '/directivas'
